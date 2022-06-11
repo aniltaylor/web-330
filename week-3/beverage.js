de460fb2da3b411c2c8d5da1ada61e13a927fcba
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-11 16:03:54 -0500

    Update beverage.js

--- week-3/beverage.js
+++ week-3/beverage.js
@@ -7,12 +7,4 @@ this.name = name;
 this.price = price;
 }
 }
-
-import{ Product } from "./product.js";
-export class beverage extends Product{
-constructor(name, price)
-{
-super(name, price);
-}
-}
 </script>

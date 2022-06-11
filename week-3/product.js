71798ae7876fac590bfbe0698db60a4b1a3ff853
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-11 16:01:55 -0500

    Update product.js

--- week-3/product.js
+++ week-3/product.js
@@ -7,12 +7,4 @@ this.name = name;
 this.price = price;
 }
 }
-
-import{ Product } from "./product.js";
-export class Beverage extends Product{
-constructor(name, price)
-{
-super(name, price);
-}
-}
 </script>

d5d6b4c97c96388ab842b257da1eaa3479fc9a97
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:09:26 -0500

    Update product.js

--- week-3/product.js
+++ week-3/product.js
@@ -6,9 +6,4 @@ export class Product {
   }
 }
 
-// Export all files from index.js file to limit layer issues.
-export * from "./appetizer.js";
-export * from "./beverage.js";
-export * from "./dessert.js";
-export * from "./main-course.js";
-export * from "./bill.js";
+

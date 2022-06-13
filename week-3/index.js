70bb47f00a7683adfa627a013f83c78f52ba81f6
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:09:47 -0500

    Update index.js

--- week-3/index.js
+++ week-3/index.js
@@ -1,11 +1,3 @@
-// Create/export product class with name and price parameters.
-export class Product {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
-  }
-}
-
 // Export all files from index.js file to limit layer issues.
 export * from "./appetizer.js";
 export * from "./beverage.js";

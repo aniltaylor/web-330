d086bfee6ff4448eb52d047512e29fbbddf00a9c
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:06:12 -0500

    Update dessert.js

--- week-3/dessert.js
+++ week-3/dessert.js
@@ -1,7 +1,7 @@
 // Import product class from product.js
 import { Product } from "./product";
 
-// Create Dessert class and export module.
+// Create Dessert class/export module.
 export class Dessert extends Product {
   constructor(name, price) {
     super(name, price);

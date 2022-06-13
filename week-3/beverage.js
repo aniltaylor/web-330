ad418e59e2dbc83c21e042dc8b0a4e423eb3b228
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-12 22:05:08 -0500

    Update beverage.js

--- week-3/beverage.js
+++ week-3/beverage.js
@@ -1,7 +1,7 @@
 // Import product class from product.js
 import { Product } from "./product";
 
-// Create Beverage class and export module
+// Create Beverage class/export module
 export class Beverage extends Product {
   constructor(name, price) {
     super(name, price);

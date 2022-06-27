413398d077e776c904e566536e56d39abe1c449d
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-26 21:53:05 -0500

    Add files via upload

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,24 @@
+// Import FoodModel class.
+import { FoodModel } from "./food-model.js";
+
+// Created CalorieConverter.
+export CalorieConverter {
+  // Creates an array for each food object with the static property name data.
+  static data = [
+    new FoodModel(1007, "Egg", 78),
+    new FoodModel(1008, "Apple", 95),
+    new FoodModel(1009, "Hamburger", 354),
+    new FoodModel(1010, "Fries", 400),
+    new FoodModel(1011, "Banana", 105),
+    new FoodModel(1012, "Soda", 150),
+  ];
+
+  // Use static property find method to return for array if object.name is included in user input.
+  static find(string) {
+    return this.data.filter((foodModel) => {
+      if (string.includes(foodModel.name.toLowerCase())) {
+        return foodModel;
+      }
+    });
+  }
+}
---
+++ week-4/food-model.js
@@ -0,0 +1,8 @@
+export class FoodModel {
+  // Set up three parameters:id, name, and calories in constructor
+  constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+}

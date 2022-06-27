68143c3c6e7b31584e34546e6f8ccb68df3ff5ce
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-06-26 23:22:04 -0500

    Update calorie-converter.js

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -4,13 +4,13 @@ import { FoodModel } from "./food-model.js";
 // Created CalorieConverter.
 export class CalorieConverter {
   // Creates an array for each food object with the static property name data.
- static data ( [
-    FoodModel(1007, "Egg", 78),
-    FoodModel(1008, "Apple", 95),
-    FoodModel(1009, "Hamburger", 354),
-    FoodModel(1010, "Fries", 400),
-    FoodModel(1011, "Banana", 105),
-    FoodModel(1012, "Soda", 150),
+ static data  [
+    FoodModel(1007 "Egg" 78),
+    FoodModel(1008 "Apple" 95),
+    FoodModel(1009 "Hamburger" 354),
+    FoodModel(1010 "Fries" 400),
+    FoodModel(1011 "Banana" 105),
+    FoodModel(1012 "Soda, 150),
    ] };
 
   // Use static property find method to return for array if object.name is included in user input.
